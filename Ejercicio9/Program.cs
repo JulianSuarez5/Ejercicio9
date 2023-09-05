85bbbfa5b62bea72baadfeaf7a268c5d83b49b44
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:38:52 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class NumeroPrimoYNoPrimo
+{
+    static void Main()
+    {
+        int num, divi, divisi;
+        divisi = 0;
+        Console.Write("Por favor ingrese un numero: ");
+        num = int.Parse(Console.ReadLine());
+        for (divi = 1; divi <= num; divi++)
+        {
+            if (num % divi == 0)
+            {
+                divisi = divisi + 1;
+            }
+        }
+        if (divisi == 2)
+        {
+            Console.WriteLine("El número es primo");
+        }
+        else
+        {
+            Console.WriteLine("El número no es primo");
+        }
+
+        Console.ReadKey();
+    }
+}
